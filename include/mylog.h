16bdf0c421fb4cc41bd54a46fa2ee3e2a2bf165a
wanghao <wh___@outlook.com>
2016-10-11 16:41:49 +0800

    1. increate log library

--- include/mylog.h
+++ include/mylog.h
@@ -14,8 +14,10 @@
 #include <stdio.h>
 #include <string>
 #include <string.h>
-typedef std::string stringa;
 
+#define LOG_FILE_NAME "host_log.txt"
+
+typedef std::string stringa;
 class stackclass
 {
 public:
@@ -34,36 +36,21 @@ public:
 	enum {COLUMN = 16};
 	void logbinary(char *strinfo, const char *pbyte, int nlen);
 	void logstring(const char *szformat, ...);
-	void logtofile(const char *pszlog);
+	void log(const char *pszlog);
 	friend msglog& get_log_instance();
 private:
-	msglog(FILE *fp = NULL, stringa strname = ""):
-	   m_fp(fp), m_filename(strname){
-		   char szbuff[128] = {0};
-		   GetTempPathA(128, szbuff);
-		   strcat_s(szbuff, 128, "mylog.txt");
-		   m_filename = szbuff;
-
-		   m_fp = fopen(m_filename.c_str(), "a");
-
-	   };
-	   ~msglog(){
-		   fclose(m_fp);
-		   m_fp = NULL;
-	   }
-
-private:
-	FILE *m_fp;
-	stringa m_filename;
-	stringa m_strlog;
+	msglog(){}
+	~msglog(){}
 };
 #endif
 
-//{{ 此处为log的接口
-
-//FUN_IN 宏没有直接调用日志输出，是通过一个类的构造析构函数实现的
-#define FUN_IN(fun_name) stackclass sclss_##fun_name(#fun_name);
-//不定长参数就要加括号，定长参数可以不加
-#define LOG_INF(loginfo) get_log_instance().logstring loginfo;
-#define LOG_BIN(strinfo, pbdata, nlen) get_log_instance().logbinary(strinfo, pbdata, nlen);
-//}}
+//此处为log的接口
+#if (defined LOG_TO_FILE) || (defined LOG_TO_STD)
+	#define FUN_IN(fun_name) stackclass sclss_##fun_name(#fun_name)
+	#define LOG_INF(loginfo) get_log_instance().logstring loginfo
+	#define LOG_BIN(strinfo, pbdata, nlen) get_log_instance().logbinary(strinfo, pbdata, nlen)
+#else
+	#define FUN_IN(fun_name)
+	#define LOG_INF(loginfo)
+	#define LOG_BIN(strinfo, pbdata, nlen)
+#endif
--- mylog.cpp
+++ mylog.cpp
@@ -1,7 +1,40 @@
+#include "stdafx.h"
 #include "mylog.h"
 #include <time.h>
 
 int g_fun_level = 1;
+
+void msglog::log(const char *pszlog)
+{
+	time_t t;
+	time(&t);
+
+	struct tm* time_s;
+	time_s = localtime(&t);
+
+	char logs[10240] = {0};
+	sprintf(logs, "%02d/%02d %02d:%02d:%02d [%d--%d] %s", 
+		time_s->tm_mday, time_s->tm_mon + 1, time_s->tm_hour, time_s->tm_min, time_s->tm_sec,
+		GetCurrentProcessId(), GetCurrentThreadId(), pszlog);
+
+#ifdef LOG_TO_STD
+	OutputDebugStringA(pszlog);
+	puts(pszlog);
+#endif
+
+#ifdef LOG_TO_FILE
+	char szfilename[128] = {0};
+	GetTempPathA(128, szfilename);
+	strcat_s(szfilename, 128, LOG_FILE_NAME);
+	FILE *m_fp;
+	m_fp = fopen(szfilename, "a");
+
+	fwrite(logs, 1, strlen(logs), m_fp);
+	fputs("\n\n", m_fp);
+	fclose(m_fp);
+	m_fp = NULL;
+#endif
+}
 
 stackclass::stackclass(const char *fun_name):m_strlog(fun_name),m_level(g_fun_level++)
 {
@@ -11,7 +44,7 @@ stackclass::stackclass(const char *fun_name):m_strlog(fun_name),m_level(g_fun_le
 		strcat(szTab, "   ");
 	sprintf(szTemp, "FUN %s-> %s()", szTab, m_strlog.c_str());
 
-	get_log_instance().logtofile(szTemp);
+	get_log_instance().log(szTemp);
 }
 
 stackclass::~stackclass()
@@ -22,34 +55,18 @@ stackclass::~stackclass()
 	for(int i = 0; i < m_level; ++i)
 		strcat(szTab, "   ");
 	sprintf(szTemp, "FUN %s<- %s()", szTab, m_strlog.c_str());
-	get_log_instance().logtofile(szTemp);
+	get_log_instance().log(szTemp);
 	g_fun_level--;
 }
 
 void msglog::logstring(const char *szformat, ...)
-{
-#if 0
-	simple_va_fun(4, 23, 43, 54, 32);
-	void simple_va_fun(int i, ...) 
-	{ 
-		va_list arg_ptr; 
-		int j=0; 
-		va_start(arg_ptr, i); 
-		j=va_arg(arg_ptr, int); 
-		int k = va_arg(arg_ptr, int);
-		va_end(arg_ptr); 
-		printf("%d %d %d\n", i, j, k); 
-		// 4 23 43
-		return; 
-	}
-#endif	
-	
+{	
 	va_list valist;
 	char szbuff[128] = {0};
 	va_start(valist, szformat);
 	_vsnprintf(szbuff, 128, szformat, valist);
 	va_end(valist);
-	logtofile(szbuff);
+	log(szbuff);
 }
 
 void msglog::logbinary(char *strinfo, const char *pbyte, int nlen)
@@ -68,24 +85,8 @@ void msglog::logbinary(char *strinfo, const char *pbyte, int nlen)
 		}
 	}
 
-	logtofile(strinfo);
-	logtofile(pbuff);
-}
-
-void msglog::logtofile(const char *pszlog)
-{
-	time_t t;
-	time(&t);
-
-	struct tm* time_s;
-	time_s = localtime(&t);
-
-	char log[1024] = {0};
-	sprintf(log, "%02d/%02d %02d:%02d:%02d [%d--%d] %s", 
-		time_s->tm_mday, time_s->tm_mon + 1, time_s->tm_hour, time_s->tm_min, time_s->tm_sec,
-		GetCurrentProcessId(), GetCurrentThreadId(), pszlog);
-	fwrite(log, 1, strlen(log), m_fp);
-	fputs("\n\n", m_fp);
+	log(strinfo);
+	log(pbuff);
 }
 
 msglog &get_log_instance()
