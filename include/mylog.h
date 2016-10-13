020be836a7c59650fcc32c48035bbc3348629ca0
wanghao <wh___@outlook.com>
2016-10-13 17:14:32 +0800

    1. say how to use

--- include/mylog.h
+++ include/mylog.h
@@ -1,11 +1,11 @@
 #ifndef _MYLOG_H
 #define _MYLOG_H
 /*
- * FUN_IN(funname); add()auto
-
  * LOG_INF(("this is num :%d", 100));
 
- * LOG_BIN("instruct", p, nlen);
+ * FUN_IN(funname); add()auto
+
+ * LOG_BIN("instruct:", p, nlen);
  
  * log for error need add file and line information
  */
@@ -60,7 +60,7 @@ private:
 
 //此处为log的接口
 #if (defined LOG_TO_FILE) || (defined LOG_TO_STD)
-	#define FUN_IN(fun_name) stackclass sclss_##fun_name(#fun_name)
+	#define FUN_IN(fun_name) get_log_instance();stackclass sclss_##fun_name(#fun_name)
 	#define LOG_INF(loginfo) get_log_instance().logstring loginfo
 	#define LOG_BIN(strinfo, pbdata, nlen) get_log_instance().logbinary(strinfo, pbdata, nlen)
 #else
