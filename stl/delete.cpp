81fbd067dd8004c596932a0811ea179b05281e91
wanghao <wh___@outlook.com>
2017-04-12 09:56:28 +0800

    1. add delete

--- stl/delete.cpp
+++ stl/delete.cpp
@@ -1,50 +1,46 @@
 /*
- 《effective stl》条款7
-  stl容器中如果保存指针，需要手动释放，如果使用for循环，不是太方便，可以使用for_each,但for_each的使用需要注意
-  一些问题...
- */
- 
-#include <vector>
-#include <algorithm>
-#include <vld.h>
-using namespace std;
+stl 不同的容器在不同的方式删除元素时，没有统一的写法，
+需要总结一下
 
-/*
-	如果T类型是继承而来的，容易出现问题（base class 未声明析构函数为虚函数，传入的类型又是父类）。
-	故可以将模板化后延。
-	但仍然有异常安全问题。这个可以使用智能指针代替。
-	unary_function指明 operator()只有一个参数
-	第一个参数是Operator()的第一个参数，第二个参数是其返回值类型。
-	使用unary_function是为了能使用函数甜酸器(notl, not2, bind1st, bind2nd)
- */
+*/
 
-//C++ 仿函数
- //template<typename T>
- struct deleteobj //条款40描述了为什么 这里有个继承
-	// :public unary_function<const T*, void>
- {
-	 //将模板化加在这里
-	 template<typename T>
-	 void operator() (const T* ptr) const
-	 {
-		 delete ptr;
-	 }
- };
- 
- class people
- {};
+int main()
+{
+	//1 把容器中所有值为X的对象都去掉。
+	//如果是一个连接内存容器(vector\deque\string)。最好的方法是earse-remove惯用法
+	vector<int> c;
+	c.erase(remove(c.begin(), c.end(), 3), c.end());
+	//这方法也适合于List,但list的成员函数remove更高效。
+	//如果是标准关联窗口，解决的方法是调用erase();
 
- int main()
- {
-	people *p;
-	std::vector<people*> temp;
-	for (int i = 0; i < 10; i++)
+
+	//2. 修改一下问题，不是删除每个特定值的物体，而是判断式badvalue(x)
+	//对于序列容器，（vector string deque list）,我们要做的是使用remove_if
+	//对于标准关联容器，使用如下方法
+	for (;;)
 	{
-		p = new people;
-		temp.push_back(p);
+		if (badValue(*i))
+			c.erase(i++);
+		else
+			++i;
 	}
-	//使用for_each比 for循环要方便
-	for_each(temp.begin(), temp.end(), deleteobj());
-	 
-	return 0;
- }
+
+	//3 再次修改为需要将数据打印，关联容器好说，但序列容器（vector/string/ deque）就要修改为循环方式了
+	//对一list 既可以用这种，也可以用上面的方法
+	for (;;)
+	{
+		if (badValue(*i))
+		{
+			//Log
+			//为什么这里不同？因为序列容器删除元素，不仅当前iterator失效，后面的所有
+			//iterator都会失效。但返回值会返回一个我们需要的iterator。故采用这种方式
+			i = c.erase(i);
+		}
+		else ++i;
+	}
+
+	/*
+	为什么remove要与erase配合使用，是因为remove并不会真正删除元素，而是使用挤压的方式remove掉
+	也就是说remove后容器的大小不会变。
+	*/
+}
